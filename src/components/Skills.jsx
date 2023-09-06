11088577a546ba9b1becbf249216c2a412d95a08
MazharSolkar <mazharsolkar65@gmail.com>
2023-09-07 02:21:33 +0530

    skills comp updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,29 +10,29 @@ const Skills = () => {
       </h1>
       <div className='flex flex-wrap justify-center items-center'>
         <div className='border-2 border-cyan-500 m-4 rounded-full shadow-lg shadow-cyan-500/50 overflow-hidden'>
-          <FaReact className='text-7xl md:text-8xl text-white-500 p-2 bg-cyan-500' />
+          <FaReact className='text-6xl md:text-7xl  text-white-500 p-2 bg-cyan-500' />
         </div>
 
         <div className='border-2 border-purple-500 m-4 rounded-full shadow-lg shadow-purple-500/50 overflow-hidden'>
-          <SiRedux className='text-7xl md:text-8xl text-white-500 p-2 bg-purple-500' />
+          <SiRedux className='text-6xl md:text-7xl  text-white-500 p-2 bg-purple-500' />
         </div>
         <div className='border-2 border-red-500 m-4 rounded-full shadow-lg shadow-red-500/50 overflow-hidden'>
-          <SiJest className='text-7xl md:text-8xl text-white-500 p-2 bg-red-500' />
+          <SiJest className='text-6xl md:text-7xl  text-white-500 p-2 bg-red-500' />
         </div>
         <div className='border-2 border-teal-500 m-4 rounded-full shadow-lg shadow-teal-500/50 overflow-hidden'>
-          <SiTailwindcss className='text-7xl md:text-8xl text-white-500 p-2 bg-teal-500' />
+          <SiTailwindcss className='text-6xl md:text-7xl  text-white-500 p-2 bg-teal-500' />
         </div>
         <div className='border-2 border-yellow-500 m-4 rounded-full shadow-lg shadow-yellow-500/50 overflow-hidden'>
-          <DiJavascript1 className='text-7xl md:text-8xl text-white-500 p-2 bg-yellow-500' />
+          <DiJavascript1 className='text-6xl md:text-7xl  text-white-500 p-2 bg-yellow-500' />
         </div>
         <div className='border-2 border-orange-500 m-4 rounded-full shadow-lg shadow-orange-500/50 overflow-hidden'>
-          <FaHtml5 className='text-7xl md:text-8xl text-white-500 p-2 bg-orange-500' />
+          <FaHtml5 className='text-6xl md:text-7xl  text-white-500 p-2 bg-orange-500' />
         </div>
         <div className='border-2 border-blue-500 m-4 rounded-full shadow-lg shadow-blue-500/50 overflow-hidden'>
-          <FaCss3Alt className='text-7xl md:text-8xl text-white-500 p-2 bg-blue-500' />
+          <FaCss3Alt className='text-6xl md:text-7xl  text-white-500 p-2 bg-blue-500' />
         </div>
         <div className='border-2 border-white m-4 rounded-full shadow-lg shadow-white/50 overflow-hidden'>
-          <SiGithub className='text-7xl md:text-8xl text-black p-2 bg-white' />
+          <SiGithub className='text-6xl md:text-7xl  text-black p-2 bg-white' />
         </div>
       </div>
     </section>
